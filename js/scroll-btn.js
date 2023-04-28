271d1a0cd24ba6ef8f07fd23a29d1f0459c2cfe3
Mostafa Elsyad <116807274+MostafaElsyad@users.noreply.github.com>
2023-04-28 14:28:17 +0200

    Update scroll-btn.js

--- js/scroll-btn.js
+++ js/scroll-btn.js
@@ -1,9 +1,21 @@
 const scrollBTN = document.getElementById('scrollBTN');
 
+function FadeIn() {
+    scrollBTN.style.bottom = '6%';
+}
+
+function FadeOut() {
+    scrollBTN.style.bottom = '-10%';
+}
+
 onscroll = function() {
-  if(scrollY >= 400) {
-    scrollBTN.style.display = 'flex';
-  }else {
-    scrollBTN.style.display = 'none';
-  }
+    if(scrollY >= 400) {
+        setTimeout(FadeIn(), 300);
+    }else {
+        setTimeout(FadeOut(), 300);
+    }
+}
+
+scrollBTN.onclick = function() {
+    scroll(0, 0);
 }
