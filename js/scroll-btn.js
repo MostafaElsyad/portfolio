e34d13b8e8b39ec075e63efba48e3b0f6efe9815
Mostafa Elsyad <116807274+MostafaElsyad@users.noreply.github.com>
2023-04-28 13:52:30 +0200

    Update scroll-btn.js

--- js/scroll-btn.js
+++ js/scroll-btn.js
@@ -1,1 +1,9 @@
+const scrollBTN = document.getElementById('scrollBTN');
 
+onscroll = function() {
+  if(scrollY >= 400) {
+    scrollBTN.style.display = 'flex';
+  }else {
+    scrollBTN.style.display = 'none';
+  }
+}
